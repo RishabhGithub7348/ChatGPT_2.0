a224ed71a646ad1e346d01ce8057a66fe889229b
Rishabh Maurya <75687649+RishabhGithub7348@users.noreply.github.com>
2023-04-30 20:30:42 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions)
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
       <SessionProvider session={session} >
      {!session ? (
        <Login/>
